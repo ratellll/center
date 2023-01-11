59509730e2fe47c7585bb4ca0abc14ce2f280459
ratellll <latellbin@naver.com>
2023-01-11 23:18:46 +0900

    core first time

---
+++ src/main/java/sup/center/CenterApplication.java
@@ -0,0 +1,13 @@
+package sup.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterApplication.class, args);
+	}
+
+}
